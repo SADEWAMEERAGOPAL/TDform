9eeb57dbf553b14e8584126c871e91aa4f0b8277
SADEWAMEERAGOPAL <SADEWADMEERA76@GMAIL.COM>
2026-01-11 14:52:17 +0530

    ngtd-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TDform';
+ 
+  @ViewChild('SignUpForm')SignUpForm!: NgForm;
+
+geteditObj={
+  ModeOfContact : "Phone",
+answer: "Dog",
+email: "jhon@gmail.com",
+isSubscribe: true,
+secretquestions: "fevPet",
+username: "jhondoe"
+}
+
+  MocArr=[
+    {
+      ContactMode: "Phone", id: 1,
+    },
+    {
+      ContactMode: "Email", id: 2,
+    },
+  ]
+
+  OnSignUp(){
+     if(this.SignUpForm.valid){
+         console.log(this.SignUpForm)
+         console.log(this.SignUpForm.value)
+     }
+    
+    this.SignUpForm.reset()
+  }
+
+  OnEdit(){
+     this.SignUpForm.form.patchValue(this.geteditObj)
+}
 }
